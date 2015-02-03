4606b0b2d973bc1ab8c964c5e07fc07143678f5b
Nikolai <srgunny@gmail.com>
2015-02-03 10:38:58 -0600

    Part2

---
+++ Part2/linked_list2.c
@@ -0,0 +1,18 @@
+//********************************************************
+// Name			: Nicholas Warner and Jennings Fairchild
+// Date			: 3 February 2015
+// Subject		: CSCE 312-501
+// Assignment	: Machine Problem 1
+// Updated		: 3 February 2015
+// Description	: Memory Management through Linked Lists
+//********************************************************
+
+#include "linked_list2.h"
+
+void 	Init (int M, int b, int t){} 
+void 	Destroy (){} 		 
+int 	Insert (int key,char * value_ptr, int value_len){}
+int 	Delete (int key){}
+char* 	Lookup (int key){}
+void 	PrintList (){}
+
