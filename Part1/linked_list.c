3a14566bb391bca934d9d26df8829e50b26ca13a
Nikolai <srgunny@gmail.com>
2015-02-04 08:21:32 -0600

    Part 1

--- Part1/linked_list.c
+++ Part1/linked_list.c
@@ -8,11 +8,31 @@
 //********************************************************
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "linked_list.h"
 
-void 	Init (int M, int b){} 
-void 	Destroy (){} 		 
-int 	Insert (int key,char * value_ptr, int value_len){}
-int 	Delete (int key){}
-char* 	Lookup (int key){return NULL;}
-void 	PrintList (){}
+void Init ( int M, int b )	// M = amount of total memory bytes, b = basic block size/memory in each list node
+{
+	head = malloc( M );
+	basicBlockSize = b;
+	Node temp;
+	head->next = &temp;
+	for ( int i = 0; i < M/b; ++i )
+	{
+	}
+}
+
+void Destroy ()
+{}
+void memset( char* buf, int b, int sizeOfBuf )
+{}
+int Insert ( int key, char* value_ptr, int value_len )
+{}
+int Delete ( int key )
+{}
+char* Lookup ( int key )
+{
+	return NULL;
+}
+void PrintList ()
+{}
